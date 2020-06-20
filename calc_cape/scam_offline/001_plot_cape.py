c522fdab9b14a15e1d7508c6461f176e7c3457cf
zhangtao <tzhang@bnl.gov>
2020-06-19 18:18:59 -0700

    set tikeadd=0.5;use divt instead of divs;

--- calc_cape/scam_offline/001_plot_cape.py
+++ calc_cape/scam_offline/001_plot_cape.py
@@ -5,13 +5,21 @@ import sys
 
 #data = np.loadtxt("goamazon_maxi.txt")
 data = np.loadtxt("goamazon_cape.txt")
-#data1 = np.loadtxt("/global/homes/z/zhangtao/cape/scam/cesm1_2_2/CAPE_new/run/goamazon_dcape.txt")
-#diff = data - data1
+data1 = np.loadtxt("goamazon_dcape.txt")
+data2 = np.zeros(data.shape[0])
 
-plt.figure(figsize=(12,4))
-#plt.plot(diff[:250])
-#print(diff[:250])
-plt.plot(data[:250])
+fig, ax = plt.subplots(1,1,figsize=(7,4))
+ax.plot(data / 1000.0, label="CAPE (kJ/kg)")
+ax.plot(data1*10 / 1000.0, label="dCAPE (10*kJ/kg/hr)")
+ax.axhline(y=0.0, linestyle='-')
 
+ax.set_ylim(-5,10)
+#ax[1].plot(data1)
+
+#ax[0].set_title("CAPE")
+#ax[1].set_title("dCAPE")
+
+#plt.tight_layout()
+ax.legend()
 plt.show()
 
